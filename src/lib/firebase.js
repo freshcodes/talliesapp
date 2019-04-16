bec66cb37bf5acd3b3315ff63cf7a5a489c9da68
Brandon Aaron <hello.brandon@aaron.sh>
2019-04-16 14:41:52 -0400

    workaround firefox private mode issue

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,18 +13,21 @@ const firebaseConfig = {
 }
 firebase.initializeApp(firebaseConfig)
 
-firebase.firestore().enablePersistence().catch(err => {
-  if (err.code === 'failed-precondition') {
-    console.debug('Could not enable persistence because there are multiple tabs open', err)
-    // Multiple tabs open, persistence can only be enabled
-    // in one tab at a a time.
-  } else if (err.code === 'unimplemented') {
-    console.debug('Could not enable persistence because the browser does not support it', err)
-    // The current browser does not support all of the
-    // features required to enable persistence
-  } else {
-    console.debug('Unknown persistence error:', err)
-  }
-})
+// https://github.com/firebase/firebase-js-sdk/issues/801#issuecomment-388371097
+if ('serviceWorker' in navigator) {
+  firebase.firestore().enablePersistence().catch(err => {
+    if (err.code === 'failed-precondition') {
+      console.debug('Could not enable persistence because there are multiple tabs open', err)
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+    } else if (err.code === 'unimplemented') {
+      console.debug('Could not enable persistence because the browser does not support it', err)
+      // The current browser does not support all of the
+      // features required to enable persistence
+    } else {
+      console.debug('Unknown persistence error:', err)
+    }
+  })
+}
 
 export default firebase
