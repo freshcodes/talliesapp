f00432252a0e41aa40e77483ba23bfdfa3656ac2
Brandon Aaron <hello.brandon@aaron.sh>
2019-11-18 10:58:01 -0500

    Update firebase
    Also updated firebaseui but could not move to the latest because of an import issue.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,17 +14,10 @@ const firebaseConfig = {
 }
 firebase.initializeApp(firebaseConfig)
 
-// https://github.com/firebase/firebase-js-sdk/issues/801#issuecomment-388371097
 if ('serviceWorker' in navigator) {
-  firebase.firestore().enablePersistence().catch(err => {
-    if (err.code === 'failed-precondition') {
-      console.debug('Could not enable persistence because there are multiple tabs open', err)
-      // Multiple tabs open, persistence can only be enabled
-      // in one tab at a a time.
-    } else if (err.code === 'unimplemented') {
+  firebase.firestore().enablePersistence({ synchronizeTabs: true }).catch(err => {
+    if (err.code === 'unimplemented') {
       console.debug('Could not enable persistence because the browser does not support it', err)
-      // The current browser does not support all of the
-      // features required to enable persistence
     } else {
       console.debug('Unknown persistence error:', err)
     }
